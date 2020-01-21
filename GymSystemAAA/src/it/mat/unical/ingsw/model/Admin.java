0049656d0ef81e5bbc62280254c932f8b75521af
rimanto98 <marcogrande1998@gmail.com>
2020-01-21 16:26:01 +0100

    non mi ricordo che ho fatto

--- GymSystemAAA/src/it/mat/unical/ingsw/model/Admin.java
+++ GymSystemAAA/src/it/mat/unical/ingsw/model/Admin.java
@@ -1,6 +1,10 @@
 package it.mat.unical.ingsw.model;
 
+import java.util.ArrayList;
+
 public class Admin extends Utente{
+	
+	ArrayList <String> richieste;	//inserisco le richieste per modiche ai corsi eccetera
 	
 	public Admin() {
 		super("Admin", "Admin", "admin@admin.com", "admin");
