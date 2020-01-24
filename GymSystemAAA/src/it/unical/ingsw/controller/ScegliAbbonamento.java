d3c53a6ab39a8cadca22b49187a84e2f9ec50307
Antonino Scarpelli <n7scarpi@gmail.com>
2020-01-24 15:16:39 +0100

    commento

--- GymSystemAAA/src/it/unical/ingsw/controller/ScegliAbbonamento.java
+++ GymSystemAAA/src/it/unical/ingsw/controller/ScegliAbbonamento.java
@@ -25,6 +25,7 @@ public class ScegliAbbonamento extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//
 	}
 
 	
