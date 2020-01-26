e212df2ae02c2f2abd54b915dbd2a467aaad7325
Antonino Scarpelli <n7scarpi@gmail.com>
2020-01-26 15:29:27 +0100

    Initial commit

--- GymSystemAAA/src/it/unical/ingsw/controller/ScegliAbbonamento.java
+++ GymSystemAAA/src/it/unical/ingsw/controller/ScegliAbbonamento.java
@@ -23,7 +23,7 @@ public class ScegliAbbonamento extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		//
 	}
 
 	
